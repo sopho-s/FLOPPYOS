f1554700916df318cf5b045a959f46510a32f5ee
sopho-s <woodsnicholas01@gmail.com>
2024-03-06 15:35:51 +0000

    made adder application

---
+++ src/FLOPPYOS/Adder.asm
@@ -0,0 +1,47 @@
+bits 16
+
+org 0x9000
+startup:
+    mov bx, startmessage
+    mov ah, 2
+    int 0x42
+    mov ah, 5
+    int 0x42
+    ; waits for a number
+    mov ah, 0
+    int 0x16
+    ; stores first digit
+    push ax
+    mov ah, 1
+    int 0x42
+    mov ah, 5
+    int 0x42
+    ; waits for another number
+    mov ah, 0
+    int 0x16
+    ; stores second digit
+    push ax
+    mov ah, 1
+    int 0x42
+    mov ah, 5
+    int 0x42
+    ; pops both digits
+    pop ax
+    pop bx
+    ; converts the ascii values to numbers
+    sub bl, 0x30
+    sub al, 0x30
+    ; adds numbers together
+    add bl, al
+    xor bh, bh
+    ; prints them
+    mov ah, 4
+    int 0x42
+    mov ah, 5
+    int 0x42
+    ; returns to terminal
+    mov ah, 2
+    int 0x96
+
+;data
+startmessage db "Please enter two numbers", 0
--- src/FLOPPYOS/Build.sh
+++ src/FLOPPYOS/Build.sh
@@ -6,7 +6,7 @@ fallocate -l 1474560 test.img
 nasm "Boot.asm" -f bin -o "Boot.bin"
 nasm "Kernel.asm" -f bin -o "Kernel.bin"
 nasm "Terminal.asm" -f bin -o "Terminal.bin"
-nasm "Test.asm" -f bin -o "Test.bin"
+nasm "Adder.asm" -f bin -o "Adder.bin"
 
 cat "Boot.bin"  > "OS.bin"
 
@@ -16,7 +16,7 @@ sudo mkdir /media/floppy1
 sudo mount -o loop test.img /media/floppy1/
 sudo cp ./Kernel.bin /media/floppy1/
 sudo cp ./Terminal.bin /media/floppy1/
-sudo cp ./Test.bin /media/floppy1/
+sudo cp ./Adder.bin /media/floppy1/
 sudo umount /media/floppy1/
 dd status=noxfer conv=notrunc if=OS.bin of=test.img
 qemu-system-i386 -m 1 -fda test.img
